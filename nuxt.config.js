01872475ed4848f0a6eb3aa5a5874dadcef48dd0
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2022-10-20 16:41:10 -0300

    Link da fonte, falta ajustar link do script do gráfico

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-           { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inspiration&display=swap' }
+           { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap' }
+           // { type:"text/javascript", src:"https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js"}
           ],
   },
 
