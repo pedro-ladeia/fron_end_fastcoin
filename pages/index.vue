80031c3d8f9324a1e866485890e648d904697158
Aluno SENAI LAURO <aluno@SENAI-LAURO.LOCAL>
2022-10-24 21:34:39 -0300

    Images

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-  
   <v-row>
     <RodaJequiti />
     <NavBar />
@@ -72,21 +71,40 @@
 
     <!-- Caroulsel  -->
 
-    <v-carousel
-      class="mt-16"
-      cycle
-      height="40rem"
-      hide-delimiter-background
-      show-arrows-on-hover
-    >
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-row class="fill-height" align="end" justify="center">
-          <img :src="colors[i]" width="100%" />
+    <v-row class="noticeWrapper">
+      <v-col col="12" md="12">
+        <div class="titleSection mt-16">
+          <img :src="btcGirando" alt="" width="90rem" />
+          <h1 class="ml-7 mr-1 mt-4">Notices</h1>
+          <img :src="btcGirando" alt="" width="90rem" />
+        </div>
 
-          <div class="text-h2 mb-15">{{ slide }} Slide</div>
-        </v-row>
-      </v-carousel-item>
-    </v-carousel>
+      
+        <v-carousel
+          class="mt-16"
+          cycle
+          height="40rem"
+          hide-delimiter-background
+          show-arrows-on-hover
+        >
+          <v-carousel-item v-for="(slide, i) in slides" :key="i">
+            <v-row class="fill-height" align="end" justify="center">
+              <img :src="colors[i]" width="100%"/>
+
+              <div class="text-h2 mb-10 ml-4">
+                <a href="#" target="_blank" rel="noopener noreferrer">{{
+                  slide
+                }}</a>
+              </div>
+            </v-row>
+          </v-carousel-item>
+        </v-carousel>
+
+
+      </v-col>
+    </v-row>
+
+    
   </v-row>
 </template>
 
@@ -95,7 +113,7 @@
 
 .initTextWrapper .mainTitle {
   font-size: 32pt;
-  font-family: 'Roboto', sans-serif !important; /*Não tá indo essa porra*/
+  font-family: 'Roboto', sans-serif !important;
   font-style: italic;
 }
 
@@ -133,42 +151,68 @@
 
 .text-h2 {
   position: absolute;
-
+  font-size: 10pt !important;
   background: rgba(255, 255, 255, 0) !important;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1) !important;
   backdrop-filter: blur(3.4px) !important;
   border: none !important;
 }
+
+.text-h2 a {
+  color: rgba(255, 255, 255, 0.76);
+  text-decoration: none;
+}
+
+.text-h2 a:hover {
+  color: #fff;
+}
+
+
+
+.noticeWrapper .titleSection {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.noticeWrapper .titleSection h1 {
+  font-size: 40pt;
+  font-family: 'Roboto', sans-serif !important;
+  font-style: italic;
+}
+
+
 </style>
 
 
 <script>
 import NavBar from '~/components/navBar.vue'
-import RodaJequiti from "../components/rodaJequiti.vue"
+import RodaJequiti from '../components/rodaJequiti.vue'
 
 export default {
   name: 'IndexPage',
   data() {
     return {
+      btcGirando: 'img/btcGirando.gif',
       colors: [
-        'img/imgtest.jpg',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
+        'img/imgTest3.jpg',
+        'img/imgTest2.jpg',
+        'img/imgTest5.jpg',
+        'img/imgTest4.jpg',
+        'img/imgTest6.jpg',
         'deep-purple accent-4',
       ],
       slides: [
         'Os touros Cardano perdem o fôlego após o hard fork Vasil: Queda de preço de 40% ADA.',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth',
-      ]
+        'Binance vai queimar todas as taxas de negociação do LUNC após feedback da comunidade.',
+        '4 sinais que indicam alta para o bitcoin na reta final de 2022 ',
+        'Dogecoin sobe 18% após Musk dizer que Tesla vai aceitar moeda como pagamento',
+        'USDT chegará a milhares de caixas eletrônicos no Brasil em novembro',
+      ],
     }
   },
   components: { NavBar, RodaJequiti },
 
-  
   methods: {
     /*
     typeWriter () {
@@ -193,6 +237,6 @@ export default {
       }, 1300);
   }
 */
-  }
+  },
 }
 </script>
