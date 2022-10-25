28fa5d0482d23147ffc3d65daf62307bf856d34b
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2022-10-25 17:04:38 -0300

    Painel do tv

---
+++ components/mainGraphs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mainWrapper">
+    <div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
+  {
+  "width": "100%",
+  "height": "100%",
+  "defaultColumn": "overview",
+  "screener_type": "crypto_mkt",
+  "displayCurrency": "USD",
+  "colorTheme": "dark",
+  "locale": "en",
+  "isTransparent": true
+}
+  </script>
+</div>
+  </div>
+
+<!-- TradingView Widget END -->
+</template>
+
+<style>
+.mainWrapper {
+  height: 30rem;
+}
+</style>
